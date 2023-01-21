ac4c63a72a13efc0566597241a233bba7fd3c3fc
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-01-21 10:51:28 -0500

    test

--- src/Testing.java
+++ src/Testing.java
@@ -1,7 +1,43 @@
 
 public class Testing {
+	
+	private int hello;
+	private int poop;
+	
+	public int getHello() {
+		return hello;
+	}
+
+
+
+	public void setHello(int hello) {
+		this.hello = hello;
+	}
+
+
+
+	public int getPoop() {
+		return poop;
+	}
+
+
+
+	public void setPoop(int poop) {
+		this.poop = poop;
+	}
+
+
+
+	
+	
 
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
 		
 
 	}
